0cfa09b64d9b8ed0caafba08f621d712b057aee4
VedantBandre <vedantbandre33@gmail.com>
2026-04-02 12:15:23 +0200

    svg plugin implementation

--- skyline/src/components/cards/AdditionalInfo.tsx
+++ skyline/src/components/cards/AdditionalInfo.tsx
@@ -2,25 +2,33 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { getWeather } from "../../api";
 // import WeatherIcon from "../WeatherIcon";
 import Card from "./Card";
+import Bewölkung from "/src/assets/sunrise.svg?react";
+import UV from "/src/assets/uv.svg?react";
+import Windrichtung from "/src/assets/wind.svg?react";
+import Luftdruck from "/src/assets/pressure.svg?react";
 
 type Props = {}
 
 const rows = [
     {
         label: "Bewölkung(%)",
-        value: 'cloud'
+        value: 'cloud',
+        Icon: Bewölkung
     },
     {
         label: "UV-index",
-        value: 'uv'
+        value: 'uv',
+        Icon: UV
     },
     {
         label: "Windrichtung(°)",
-        value: 'wind_degree'
+        value: 'wind_degree',
+        Icon: Windrichtung
     },
     {
         label: "Luftdruck(mb)",
-        value: 'pressure_mb'
+        value: 'pressure_mb',
+        Icon: Luftdruck
     },
 ] as const;
 
@@ -32,11 +40,20 @@ export default function AdditionalInfo ({} : Props) {
     });
 
     return (
-        <Card title="Additional Info" childrenClassName="flex flex-col gap-8">
-            {rows.map(({ label, value }) => (
+        <Card 
+        title="Additional Info" 
+        childrenClassName="flex flex-col gap-8">
+            {rows.map(({ label, value, Icon }) => (
                 <div className="flex justify-between" key={value}>
-                    <span className="text-gray-300/75">{label}</span>
-                    <span>{data.current[value]}</span>
+                    
+                    <div className="flex gap-4">
+                        <span className="text-gray-300/75">{label}</span>
+                        <Icon className="invert size-8"></Icon>
+                    </div>
+                    
+                    <span>
+                        {data.current[value]}
+                    </span>
                 </div>
             ))}
         </Card>
